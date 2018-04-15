5f750eb011ac2067508810f28571c54b7c29ef47
Андрей Володченко <a.k.volodchenko@gmail.com>
2018-04-15 14:10:29 +0300

    task 1 done

--- js/map.js
+++ js/map.js
@@ -1,11 +1,10 @@
 'use strict';
 
 var OFFER_TYPE = ['palace', 'flat', 'house', 'bungalo'];
-var OFFER_TYPE_RUS = [];
 var BRIEF_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var CHECK_TIME = ['12:00', '13:00', '14:00'];
 var PIN_WIDTH = 50;
-var PIN_HEIGHT = 70;
+// var PIN_HEIGHT = 70;
 var MIN_PRICE = 1000;
 var MAX_PRICE = 1000000;
 var MAX_ROOMS = 5;
@@ -16,22 +15,31 @@ var MIN_PIN_Y = 150;
 var MAX_PIN_Y = 300;
 var adverts = [];
 
-function countMinMax (min, max) {
+function countMinMax(min, max) {
   var minMaxRandom = min + Math.random() * (max - min);
   return Math.round(minMaxRandom);
-};
+}
 
 function shuffleArrayCondition() {
   return Math.random() - 0.5;
-};
+}
 
-function shuffleArray (array) {
+function shuffleArray(array) {
   return array.sort(shuffleArrayCondition);
-};
+}
 
+function contains(arr, elem) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === elem) {
+      return true;
+    }
+  }
+  return false;
+}
 
-function createAdvertData (piece) {
-  var advertItem;
+// Создаёт объект с данными для одного объявления
+function createAdvertData(piece) {
+  // var advertItem;
   var author = 'img/avatars/user' + '0' + (piece + 1) + '.png';
   var rentPrice = countMinMax(MIN_PRICE, MAX_PRICE);
   var offerTypeRand = OFFER_TYPE[countMinMax(0, OFFER_TYPE.length - 1)];
@@ -47,7 +55,7 @@ function createAdvertData (piece) {
 
   FEATURES.length = countMinMax(0, FEATURES.length);
 
-  return advertItem = {
+  return {
     author: {
       avatar: author
     },
@@ -59,7 +67,7 @@ function createAdvertData (piece) {
       guests: guestsAmount,
       checkin: checkinTimeRand,
       checkout: checkoutTimeRand,
-      features: FEATURES,
+      features: shuffleArray(FEATURES),
       description: description,
       photos: shuffleArray(PHOTOS),
       address: locationX + ', ' + locationY
@@ -69,16 +77,16 @@ function createAdvertData (piece) {
       y: locationY
     }
   };
-};
+}
 
+// Заполняет пустой массив объектами с объявлениями
 for (var i = 0; i < BRIEF_TITLES.length; i++) {
   adverts.push(createAdvertData(i));
 }
 
-console.log(adverts);
+// console.log(adverts);
 
-
-
+// Создает метку для объявления
 var renderPin = function (num, pin) {
   var img = pin.querySelector('img');
 
@@ -90,7 +98,8 @@ var renderPin = function (num, pin) {
   return pin;
 };
 
-function makePins () {
+// Создает метки для всех объявлений из массива
+function makePins() {
   var templateNode = document.querySelector('template');
   var pins = document.querySelector('.map__pins');
   var pin = templateNode.content.querySelector('.map__pin');
@@ -98,23 +107,22 @@ function makePins () {
 
   for (i = 0; i < BRIEF_TITLES.length; i++) {
     var card = templateNode.content.cloneNode(true);
-
     var button = card.querySelector('.map__pin');
+    pin = renderPin(i, button);
 
-
-    var pin = renderPin(i, button);
     fragment.appendChild(pin);
   }
+
   pins.appendChild(fragment);
-};
+}
 
 makePins();
 
-function createCard (num) {
+// Создает карточку с подробной информацией по объявлению из массива с объявлениями
+function createCard(num) {
   var templateNode = document.querySelector('template');
   var card = templateNode.content.cloneNode(true);
   var cardItem = card.querySelector('.map__card');
-
   var titleBlock = card.querySelector('.popup__title');
   titleBlock.textContent = adverts[num].offer.title;
 
@@ -124,22 +132,24 @@ function createCard (num) {
   var priceBlock = card.querySelector('.popup__text--price');
   priceBlock.textContent = adverts[num].offer.price + '₽/ночь';
 
-  function changeTypeLang () {
+  // Меняет значения массива с типом жилья на аналогичные на русском языке для отображения в объявлении
+  function changeTypeLang() {
     for (i = 0; i < BRIEF_TITLES.length; i++) {
       if (adverts[i].offer.type === 'palace') {
-        adverts[i].offer.type = 'Дворец'; }
-        else if (adverts[i].offer.type === 'house' ) {
-          adverts[i].offer.type = 'Дом';
-        }       else if (adverts[i].offer.type === 'flat' ) {
-          adverts[i].offer.type = 'Квартира';
-        }       else if (adverts[i].offer.type === 'bungalo' ) {
-          adverts[i].offer.type = 'Бунгало';
-        }
+        adverts[i].offer.type = 'Дворец';
+      } else if (adverts[i].offer.type === 'house') {
+        adverts[i].offer.type = 'Дом';
+      } else if (adverts[i].offer.type === 'flat') {
+        adverts[i].offer.type = 'Квартира';
+      } else if (adverts[i].offer.type === 'bungalo') {
+        adverts[i].offer.type = 'Бунгало';
+      }
     }
-  };
+  }
 
   var typeBlock = card.querySelector('.popup__type');
   changeTypeLang();
+
   typeBlock.textContent = adverts[num].offer.type;
 
   var capacityBlock = card.querySelector('.popup__text--capacity');
@@ -148,42 +158,55 @@ function createCard (num) {
   var checkBlock = card.querySelector('.popup__text--time');
   checkBlock.textContent = 'Заезд после ' + adverts[num].offer.checkin + ', выезд до ' + adverts[num].offer.checkout;
 
-  var featuresBlock = card.querySelector('.popup__features');
+  var featuresBlock = cardItem.querySelector('.popup__features');
   var featureWifi = featuresBlock.querySelector('.popup__feature--wifi');
   var featureDishwasher = featuresBlock.querySelector('.popup__feature--dishwasher');
   var featureParking = featuresBlock.querySelector('.popup__feature--parking');
   var featureWasher = featuresBlock.querySelector('.popup__feature--washer');
   var featureElevator = featuresBlock.querySelector('.popup__feature--elevator');
   var featureConditioner = featuresBlock.querySelector('.popup__feature--conditioner');
-  console.log(featuresBlock);
+  var emptyFeaturesBlock = featuresBlock.cloneNode();
+
+  // console.log(emptyFeaturesBlock);
+  // console.log(adverts[0].offer.features);
+  // console.log(contains (adverts[0].offer.features, 'dishwasher'));
 
   for (i = 0; i < adverts[num].offer.features.length; i++) {
-      featuresBlock.appendChild(featureWifi);
-  featuresBlock.appendChild(featureDishwasher);
-  featuresBlock.appendChild(featureParking);
-  featuresBlock.appendChild(featureWasher);
-  featuresBlock.appendChild(featureElevator);
-  featuresBlock.appendChild(featureConditioner);
-    console.log(adverts[num].offer.features[i]);
-    if ((adverts[num].offer.features[i].includes('wifi'))) {
-      featuresBlock.removeChild(featureWifi);
-    } else if ((adverts[num].offer.features[i].includes('dishwasher'))) {
-      featuresBlock.removeChild(featureDishwasher);
-    } else if ((adverts[num].offer.features[i].includes('parking'))) {
-      featuresBlock.removeChild(featureParking);
-    } else if ((adverts[num].offer.features[i].includes('washer'))) {
-      featuresBlock.removeChild(featureWasher);
-    } else if ((adverts[num].offer.features[i].includes('elevator'))) {
-      featuresBlock.removeChild(featureElevator);
-    } else if ((adverts[num].offer.features[i].includes('conditioner'))) {
-      featuresBlock.removeChild(featureConditioner);
+    // console.log(featuresBlock);
+    // console.log(emptyFeaturesBlock);
+    // console.log(adverts[num].offer.features[i]);
+    // console.log(contains (adverts[num].offer.features, 'dishwasher'));
+
+    if (contains(adverts[num].offer.features, 'wifi')) {
+      emptyFeaturesBlock.appendChild(featureWifi);
     }
 
+    if (contains(adverts[num].offer.features, 'dishwasher')) {
+      emptyFeaturesBlock.appendChild(featureDishwasher);
+    }
 
+    if (contains(adverts[num].offer.features, 'parking')) {
+      emptyFeaturesBlock.appendChild(featureParking);
+    }
+
+    if (contains(adverts[num].offer.features, 'washer')) {
+      emptyFeaturesBlock.appendChild(featureWasher);
+    }
+
+    if (contains(adverts[num].offer.features, 'elevator')) {
+      emptyFeaturesBlock.appendChild(featureElevator);
+    }
+
+    if (contains(adverts[num].offer.features, 'conditioner')) {
+      emptyFeaturesBlock.appendChild(featureConditioner);
+    }
   }
 
-// console.log(featureWifi);
-  // featuresBlock.textContent = adverts[num].offer.features;
+  // console.log(featuresBlock);
+  // console.log(emptyFeaturesBlock);
+  // console.log(featuresBlock);
+
+  cardItem.replaceChild(emptyFeaturesBlock, featuresBlock);
 
   var descriptionBlock = card.querySelector('.popup__description');
   descriptionBlock.textContent = adverts[num].offer.description;
@@ -204,7 +227,7 @@ function createCard (num) {
   // console.log(card);
   // console.log(cardItem);
   return cardItem;
-};
+}
 
 // createCard(0);
 // console.log(createCard(1));
@@ -213,4 +236,5 @@ var map = document.querySelector('.map');
 var mapFilters = document.querySelector('.map__filters-container');
 map.insertBefore(createCard(0), mapFilters);
 
-console.log(map);
+// console.log(map);
+

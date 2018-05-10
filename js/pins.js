241592142901a2757af0f26f03cda90f43941a3d
Андрей Володченко <a.k.volodchenko@gmail.com>
2018-05-10 20:29:28 +0300

    Улучшение отрисовки пинов

--- js/pins.js
+++ js/pins.js
@@ -20,15 +20,12 @@
     var pins = document.querySelector('.map__pins');
     var pin = templateNode.content.querySelector('.map__pin');
     var fragment = document.createDocumentFragment();
+    var trimmedArray = array.slice(0, MAX_PINS);
 
-    if (array.length - 1 > MAX_PINS) {
-      array.length = MAX_PINS;
-    }
-
-    for (var i = 0; i < array.length; i++) {
+    for (var i = 0; i < trimmedArray.length; i++) {
       var card = templateNode.content.cloneNode(true);
       var button = card.querySelector('.map__pin');
-      pin = window.renderPin(array[i], i, button);
+      pin = window.renderPin(trimmedArray[i], i, button);
       fragment.appendChild(pin);
     }
     pins.appendChild(fragment);
